1b10d6a92d586afc5ef15e8b575341a395ffa54c
M0H1K4N <nejer911@gmail.com>
2023-12-02 19:29:19 +0400

    Seperating folders

---
+++ Task2/Student-Management-App/backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://Lukinio120:yuTjtYz7d6Vaor3l@students.jipqeul.mongodb.net/Studens-Management?retryWrites=true&w=majority";
+
+/// pass:  yuTjtYz7d6Vaor3l
