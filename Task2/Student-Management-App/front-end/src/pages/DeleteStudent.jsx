1080e749cfd7ca55966e2694a473d0877aa4aa93
M0H1K4N <nejer911@gmail.com>
2023-12-03 14:46:12 +0400

    add

--- Task2/Student-Management-App/front-end/src/pages/DeleteStudent.jsx
+++ Task2/Student-Management-App/front-end/src/pages/DeleteStudent.jsx
@@ -11,14 +11,33 @@ export default function DeleteStudent() {
   const handleDeleteStudent = () => {
     setLoading(true);
     axios
-    .delete(`http://localhost:3000/students/${id}`)
-    .then(() => {
-      setLoading(false);
-      navigate('/');
-    })
-    .catch((err) => {
-      
-    })
+      .delete(`http://localhost:3000/students/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        alert("Ann error happened. Please Check console");
+        console.log(err);
+      });
   };
-  return <div>DeleteStudents</div>;
+  return (
+    <div className="p-4">
+      <Backbutton />
+      <h1 className="text-3xl my-4">Delete Student</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto ">
+        <h3 className="text-2xl">
+          Are You Sure You Want to delete this sdudent?
+        </h3>
+
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteStudent}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
 }
