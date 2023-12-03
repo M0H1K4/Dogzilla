6406269de8f5b479908b0e43d65bc6ffea1fc9f4
M0H1K4N <nejer911@gmail.com>
2023-12-03 14:23:16 +0400

    managing add, delete, edit

--- Task2/Student-Management-App/front-end/src/pages/CreateStudent.jsx
+++ Task2/Student-Management-App/front-end/src/pages/CreateStudent.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import Backbutton from "../../components/BackButton";
+import Spinner from "../../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function CreateStudent() {
+const CreateStudent = () => {
+  const [name, setName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveStudent = () => {
+    const data = {
+      name,
+      secondName,
+      birthYear,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:3000/students", data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("An error happened. Please Check console");
+        console.log(err);
+      });
+  };
+
   return (
-    <div>
-      Create student
+    <div className="p-4">
+      <Backbutton />
+      <h1 className="text-3xl my-4"> Add student</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Second Name</label>
+          <input
+            type="text"
+            value={secondName}
+            onChange={(e) => setSecondName(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Birth Year</label>
+          <input
+            type="number"
+            value={birthYear}
+            onChange={(e) => setBirthYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveStudent}>
+          Save
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
+export default CreateStudent;
--- Task2/Student-Management-App/front-end/src/pages/DeleteStudent.jsx
+++ Task2/Student-Management-App/front-end/src/pages/DeleteStudent.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import Backbutton from "../../components/BackButton";
+import Spinner from "../../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function DeleteStudent() {
-  return (
-    <div>
-      DeleteStudents
-    </div>
-  )
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleDeleteStudent = () => {
+    setLoading(true);
+    axios
+    .delete(`http://localhost:3000/students/${id}`)
+    .then(() => {
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((err) => {
+      
+    })
+  };
+  return <div>DeleteStudents</div>;
 }
